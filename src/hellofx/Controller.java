49f87679d8153728fb0aca5cb304db3782f6ed59
irwin felicien <felicienirwin@outlook.com>
2023-11-16 01:03:41 -0600

    need to fix tf for FXML

--- src/hellofx/Controller.java
+++ src/hellofx/Controller.java
@@ -1,5 +1,16 @@
 package hellofx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
     
+    @FXML
+    private textField getFName;
+
+    @FXML
+    void onBtnClick(ActionEvent event){
+
+    }
 }
